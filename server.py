df478ad66529d849bd0e9ba786097115dc135e6d
Herve Schmidt <h.schmidt@hotmail.fr>
2026-06-27 15:21:40 +0200

    fix: points overspending

--- server.py
+++ server.py
@@ -49,6 +49,9 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+    if placesRequired > int(club['points']):
+        flash('Not enough points to book this number of places.')
+        return render_template('welcome.html', club=club, competitions=competitions), 400
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -1,3 +1,13 @@
+# B1 Test si l'app crash pas avec un mauvais email
 def test_should_status_code_not_500(client):
     response = client.post('/showSummary', data={'email': 'test@example.com'})
     assert response.status_code != 500
+
+
+# B2 Test si l'app ne permet pas de réserver plus de places que disponible
+def test_should_not_book_without_enough_points(client):
+    response = client.post('/purchasePlaces', data={'competition': "Spring Festival",
+                                                    'club': "Simply Lift",
+                                                    'places': 15})
+    data = response.data.decode()
+    assert 'Great-booking complete!' not in data
